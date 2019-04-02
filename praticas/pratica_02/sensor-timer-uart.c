8f626a40fb09ff1ce5657be13ecf783d500077a6
Bruno Griep <brunogriep>
2019-04-02 19:22:11 -0300

    Pratica 2 Atividade 2

--- praticas/pratica_02/sensor-timer-uart.c
+++ praticas/pratica_02/sensor-timer-uart.c
@@ -36,9 +36,8 @@ PROCESS_THREAD(sensor_process, ev, data)
 
         int val = batmon_sensor.value(BATMON_SENSOR_TYPE_TEMP); // lê sensor
 
-        /* Insira seu código aqui */
-
-
+        buffer[buf_c++] = val;
+        buf_c &= 0x07;
 
         printf("Leu %d\n", val);
     }
@@ -51,6 +50,7 @@ PROCESS_THREAD(sensor_process, ev, data)
 PROCESS_THREAD(uart_process, ev, data)
 {
   PROCESS_BEGIN();
+  etimer_set(&et_uart, 10*CLOCK_SECOND); // a cada 10 segundos
 
   /* Insira seu código aqui */
 
@@ -59,13 +59,15 @@ PROCESS_THREAD(uart_process, ev, data)
     if(ev == PROCESS_EVENT_TIMER)  // se passaram 10 segundos
     {
         /* Insira seu código aqui */
+        etimer_reset(&et_uart); // reinicia timer
 
         int avg = 0;
         for(int i=0;i<BUF_SIZE;i++) {
             avg += buffer[i];
         }
         avg = avg/BUF_SIZE;
-        printf("Temperatura media: %d\n", avg);
+        printf("Temperatura media: %d, buf_c: %d\n", avg, buf_c);
+
     }
   }
 
